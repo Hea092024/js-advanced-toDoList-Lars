9d6415900ec9604a133dc07e37b2d88634fcac4d
Hea092024 <Hea092024@gmail.com>
2025-01-14 09:55:41 +0100

    added a few const`s to js

---
+++ script.js
@@ -0,0 +1,5 @@
+const inputField = document.querySelector("#input-field");
+const inputForm = document.querySelector("#input-form");
+const submitButton = document.querySelector("#submit-button");
+const listContainer = document.querySelector("#list-container");
+litarr = [];
